267c32c8a1119841d3a3c4e63b7f6329558b6504
nasonovaa <nasonovaa@63pokuoki.ru>
2023-01-21 20:16:06 +0400

    remove build

--- build/Model/User/UserDataE.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserE = void 0;
-class UserE {
-}
-exports.UserE = UserE;
-//# sourceMappingURL=UserDataE.js.map
--- build/Model/User/UserE copy.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserE = void 0;
-class UserE {
-}
-exports.UserE = UserE;
-//# sourceMappingURL=UserE%20copy.js.map
